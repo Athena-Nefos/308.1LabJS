befe622886c0272e9467b16fdce4b85c078fcc2d
Athena Nefos <athena.nefos@gmail.com>
2024-11-25 14:43:44 -0500

    another loop ex

--- loop308.3.js
+++ loop308.3.js
@@ -3,3 +3,11 @@
 for (let i = 10; i >=1; i--) {
     console.log(i);
 }
+
+//Output odd numbers from 1 to 10.
+
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 !== 0){
+        console.log(i);
+    }
+}
