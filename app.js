74b9a28daff63c0690db6103b0b469be81aec6d9
Athena Nefos <athena.nefos@gmail.com>
2024-11-25 14:40:33 -0500

    loop exercise

--- app.js
+++ app.js
@@ -46,3 +46,33 @@ console.log(finalResult);
 const remainder = finalResult % n4;
 
 console.log(remainder)
+
+// Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate.
+const isSum50 = (n1 + n2 + n3 + n4) == 50;
+
+// Check two: at least two odd numbers
+// Here, we use modulus to check if something is odd.
+// Since % 2 is 0 if even and 1 if odd, we can use
+// arithmetic to count the total number of odd numbers.
+const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
+
+// Check three: no number larger than 25
+// This time, we use the OR operator to check
+// if ANY of the numbers is larger than 25.
+//* const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+const isUnder25 = n1 <= 25 && n2 <= 25 && n3 <= 25 && n4 <= 25;
+
+// Check four: all unique numbers
+// This is long, and there are more efficient
+// ways of handling it with other data structures
+// that we will review later.
+const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
+
+// Here, we put the results into a single variable 
+// for convenience. Note how we negate isOver25 using
+// the ! operator. We could also have tested for 
+// "isUnder25" as an alternative.
+// * const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
+const isVAlid = isSum50 && isTwoOdd && isUnder25 && isUnique;
+// Finally, log the results.
+console.log(isValid);
---
+++ loop308.3.js
@@ -0,0 +1,5 @@
+// count down to from 10 to 1
+
+for (let i = 10; i >=1; i--) {
+    console.log(i);
+}
