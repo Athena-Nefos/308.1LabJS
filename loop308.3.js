73e76d528a246fc07bb00644898d3c5f0a015eeb
Athena Nefos <athena.nefos@gmail.com>
2024-11-25 15:19:16 -0500

    1-20 count

--- loop308.3.js
+++ loop308.3.js
@@ -1,14 +1,14 @@
 // count down to from 10 to 1
 
 for (let i = 10; i >=1; i--) {
-    console.log(i);
+ //   console.log(i);
 }
 
 //Output odd numbers from 1 to 10.
 
 for (let i = 1; i <= 10; i++) {
     if (i % 2 !== 0){
-        console.log(i);
+//        console.log(i);
     }
 }
 
@@ -16,6 +16,23 @@ for (let i = 1; i <= 10; i++) {
 
 for (let i = 1; i <= 10; i++) {
     if (i % 2 === 0) {
-        console.log(i);
+//        console.log(i);
     }
 }
+
+// Output multiples of 3, starting at 6 and ending at 60
+
+for (let i = 6; i <= 60; i += 3) {
+//    console.log(i);
+}
+
+// Output an increasing number of # symbols, from 1 to 7, as shown below.
+for (let i = 1; i <= 7; i++) {
+//    console.log('#'.repeat(i));
+
+}
+
+//write a for loop that iterates from 1 to 20
+
+for (let i = 1; i <= 20; i++)
+//    console.log(i);
