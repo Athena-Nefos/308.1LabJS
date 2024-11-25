c8180ecdb69aa7a2658436e42153734dd618d4c9
Athena Nefos <athena.nefos@gmail.com>
2024-11-25 14:47:13 -0500

    successful out put

--- loop308.3.js
+++ loop308.3.js
@@ -11,3 +11,11 @@ for (let i = 1; i <= 10; i++) {
         console.log(i);
     }
 }
+
+//Output even number from 1 to 10.
+
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
